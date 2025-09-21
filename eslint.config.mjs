d9e6527789aee03c33dc677cb06f62dbcc6cf408
Marchel <lupashtianmk@gmail.com>
2025-09-21 21:35:09 +0300

    configure ESLint for Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import pluginJest from 'eslint-plugin-jest'
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node, ...globals.jest } },
+    plugins: { jest: pluginJest },
+    settings: { jest: {} }
+  },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended
 ]
